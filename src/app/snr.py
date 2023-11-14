8f972a62d9c81db5687274e07d353498f88b72ac
otgon <otgon.petru@gmail.com>
2023-11-14 22:23:59 +0100

    Lowered logger backup count, detailed logs in snr module

--- src/app/snr.py
+++ src/app/snr.py
@@ -1,4 +1,5 @@
 from utils.config import SNR_PRICE_RANGE, SNR_BREAKOUT_FACTOR
+from utils.logger import make_log
 
 
 class SNRDetector:
@@ -8,25 +9,33 @@ class SNRDetector:
         self.breakout_factor = SNR_BREAKOUT_FACTOR
 
     def detect_clusters(self):
+        make_log("CLUSTER", 20, "clusters.log", "Initializing cluster variables")
         clusters = []
         current_cluster = []
         in_cluster = False
         cluster_max_price = cluster_min_price = None
 
+        make_log("CLUSTER", 20, "clusters.log", "Starting iteration")
         for index, row in self.data.iterrows():
             if not in_cluster:
+                make_log("CLUSTER", 20, "clusters.log", "Not in cluster")
                 in_cluster = True
                 cluster_max_price = cluster_min_price = row["Close"]
                 current_cluster = [row]
             else:
+                make_log(
+                    "CLUSTER", 20, "clusters.log", "In cluster, setting price range"
+                )
                 cluster_max_price = max(cluster_max_price, row["Close"])
                 cluster_min_price = min(cluster_min_price, row["Close"])
 
                 if self.is_breakout(row["Close"], cluster_max_price, cluster_min_price):
+                    make_log("CLUSTER", 20, "clusters.log", "Breakout detected")
                     clusters.append(current_cluster)
                     in_cluster = False
                     current_cluster = []
                 else:
+                    make_log("CLUSTER", 20, "clusters.log", "Cluster appended")
                     current_cluster.append(row)
         return clusters
 
