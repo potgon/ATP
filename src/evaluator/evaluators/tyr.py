497f3c1f6bef1e7cb1c1797e25cbcbe7fe48af8d
otgon <otgon.petru@gmail.com>
2023-11-25 22:07:57 +0100

    CDL eval now adds up if multiple are found in the last 3 rows. Max contribution is 3

--- src/evaluator/evaluators/tyr.py
+++ src/evaluator/evaluators/tyr.py
@@ -2,6 +2,7 @@ import talib as ta
 import pandas as pd
 
 from data_processing.pattern_recog import find_patterns
+from utils.config import MAX_CDL_CONTRIBUTION
 
 
 class Tyr:
@@ -15,7 +16,18 @@ class Tyr:
         ) > 5
 
     def preprocess_data(self, data: pd.DataFrame) -> pd.DataFrame:
-        patterns = ["Doji", "Engulfing", "Hammer"]
+        patterns = [
+            "Engulfing",
+            "Morning Star",
+            "Three White Soldiers",
+            "Doji",
+            "Hammer",
+            "Inverted Hammer",
+            "Evening Star",
+            "Three Black Crows",
+            "Shooting Star",
+            "Hanging Man",
+        ]
         data = find_patterns(data, patterns)
         data["RSI"] = ta.RSI(data["Close"], timeperiod=14)
 
@@ -23,13 +35,35 @@ class Tyr:
 
     def evaluate_RSI(self, data) -> int:
         return (
-            2
+            3
             if data["RSI"].iloc[-1] <= 30
-            else (-2 if data["RSI"].iloc[-1] >= 70 else 0)
+            else (-3 if data["RSI"].iloc[-1] >= 70 else 0)
         )
 
     def evaluate_CDL(self, data):
-        pass
+        alpha = 0
+        patterns = {
+            "Engulfing": 2,
+            "Morning Star": 2,
+            "Three White Soldiers": 2,
+            "Doji": 1,
+            "Hammer": 1,
+            "Inverted Hammer": 1,
+            "Evening Star": -2,
+            "Three Black Crows": -2,
+            "Shooting Star": -1,
+            "Hanging Man": -1,
+        }
+
+        for i in range(-3, 0):
+            for pattern, value in patterns.items():
+                if data[pattern].iloc[i] == 100:
+                    alpha += value
+                elif data[pattern].iloc[-1] == -100:
+                    alpha -= value
+
+        alpha = max(min(alpha, MAX_CDL_CONTRIBUTION), -MAX_CDL_CONTRIBUTION)
+        return alpha
 
     def evaluate_SNR(self):
         pass
