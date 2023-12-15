730e42bf38a43bff7f2cb8e57ade9224dec14470
Petru Otgon <otgon.petru@gmail.com>
2023-12-15 17:37:29 +0000

    Scripts package with db_test module to test db connection

---
+++ src/scripts/db_test.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, text
+from utils.config import DATABASE_URI
+
+def test_database_connection():
+    engine = create_engine(DATABASE_URI)
+
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT VERSION()"))
+            version = result.fetchone()
+            print("Database connection test successful. MySQL version:", version[0])
+            return True
+    except Exception as e:
+        print("Database connection test failed:", e)
+        return False
+
+if __name__ == "__main__":
+    if test_database_connection():
+        print("Connection to database established successfully.")
+    else:
+        print("Failed to establish connection to the database.")
