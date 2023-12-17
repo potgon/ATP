ebf0b19e9477dd30c95d748162ccee12152ae1f2
otgon <otgon.petru@gmail.com>
2023-12-17 14:37:08 +0000

    is_forex_day now accounts for global market schedule

--- src/markets/forex.py
+++ src/markets/forex.py
@@ -2,14 +2,22 @@ import datetime
 
 def is_forex_day(date=None) -> bool:
     """
-    Calculates current day (0->Monday | 6->Sunday) and returns a bool indicating whether Forex market is operating
+    Determines whether Forex market is operating
+    Currently accounting for: Closed from 10PM UTC Friday to 10PM UTC Sunday
     If no date is provided, today's date is used
     """
-    if date:
-        if date >= 5:
-            return False
-    else:
-        if datetime.datetime.today().weekday() >= 5:
-            return False
+    if not date:
+        date = datetime.datetime.utcnow()
+        
+    weekday = date.weekday
+    time = date.time()
+    
+    if weekday == 4 and time >= datetime.time(22,0):
+        return False
+    elif weekday == 5:
+        return False
+    elif weekday == 6 and time < datetime.time(22,0):
+        return False
+    
     return True
     
