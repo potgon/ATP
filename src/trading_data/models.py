0bbdbe9b3559bb468fccdbe991770c3e192e2a95
otgon <otgon.petru@gmail.com>
2023-12-19 22:33:27 +0000

    Basic Positions structure using ORM

--- src/trading_data/models.py
+++ src/trading_data/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
-class Position:
-    def __init__(models.Model):
-        close = models.FloatField()
-        atr = models.FloatField()
-        sl = models.FloatField()
-        tp = models.FloatField()
-        timestamp = models.DateTimeField(auto_now_add=True)
-        
-    def open_position(cls, close, atr):
-        return cls.objects.create(close=close, atr=atr, sl=calculated_sl, tp=calculated_tp)
+class Position(models.Model):
+    date_open = models.DateTimeField(auto_now_add=True)
+    date_close = models.DateTimeField(null=True, blank=True)
+    open_price = models.DecimalFiled(max_digits=18, decimal_places=5)
+    close_price = models.DecimalField(max_digits=18, decimal_places=5, null=True, blank=True)
+    alpha = models.IntegerField()
+    net_profit = models.DecimalField(max_digits=18, decimal_places=5, null=True, blank=True)
     
-    def close_position(self):
-        self.delete()
+    def close_position(self, close_price):
+        self.date_close = models.DateTimeField.now()
+        self.close_price = close_price
+        self.net_profit = self.calculate_net_profit()
+        self.save()
         
+    def calculate_net_profit(self):
+        return self.close_price - self.open_price
+    
