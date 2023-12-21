3e8a620401545cd6c2d163beb8c01cd2da65be73
otgon <otgon.petru@gmail.com>
2023-12-21 15:15:07 +0000

    Separated fetcher into its own module, pattern_recog module migration

---
+++ src/app/trading_data/pattern_recog.py
@@ -0,0 +1,43 @@
+import talib as ta
+import pandas as pd
+from typing import Callable
+
+
+def find_patterns(df: pd.DataFrame, pattern_list: list) -> pd.DataFrame:
+    data = df.copy()
+    for pattern in pattern_list:
+        data[pattern] = pattern_factory(pattern)(
+            data["Open"], data["High"], data["Low"], data["Close"]
+        )
+    return data
+
+
+def pattern_factory(pattern: str) -> Callable:
+    if pattern == "Engulfing":
+        return ta.CDLENGULFING
+    elif pattern == "Doji":
+        return ta.CDLDOJI
+    elif pattern == "Hammer":
+        return ta.CDLHAMMER
+    elif pattern == "Inverted Hammer":
+        return ta.CDLINVERTEDHAMMER
+    elif pattern == "Shooting Star":
+        return ta.CDLSHOOTINGSTAR
+    elif pattern == "Hanging Man":
+        return ta.CDLHANGINGMAN
+    elif pattern == "Morning Star":
+        return ta.CDLMORNINGSTAR
+    elif pattern == "Evening Star":
+        return ta.CDLEVENINGSTAR
+    elif pattern == "Harami":
+        return ta.CDLHARAMI
+    elif pattern == "Three Black Crows":
+        return ta.CDL3BLACKCROWS
+    elif pattern == "Three White Soldiers":
+        return ta.CDL3WHITESOLDIERS
+    elif pattern == "Dark Cloud Cover":
+        return ta.CDLDARKCLOUDCOVER
+    elif pattern == "Piercing":
+        return ta.CDLPIERCING
+    else:
+        raise ValueError("Unknown Pattern")
