814625dbaa6ab9800f60bd7fe4cad0e7cc61750f
otgon <otgon.petru@gmail.com>
2023-12-21 15:51:59 +0000

    Custom exception to avoid multiple tracking for a same asset

--- src/app/trading_data/tasks.py
+++ src/app/trading_data/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 
 from .models import Position
 from .services import eval_period, instantiate_algo
+from .exceptions import DuplicateAssetException
 from aws_integration.services import send_custom_metric
 from evaluation_core.factories import get_evaluator
 from evaluation_core.intervals import AlgorithmInterval
@@ -14,10 +15,14 @@ active_assets = {}
 
 @shared_task
 def manage_algorithm(algo_name, ticker):
+    if ticker in active_assets:
+        raise DuplicateAssetException(ticker)
+    
     if algo_name not in operative_algos:
         evaluator = instantiate_algo(algo_name, ticker)
         operative_algos[algo_name] = evaluator
-    else:
-        evaluator = operative_algos[algo_name] 
-    eval_period(evaluator, algo_name, ticker)                
+
+    active_assets[ticker] = algo_name
+    evaluator = operative_algos[algo_name] 
+    eval_period(evaluator, algo_name, ticker)         
     time.sleep(AlgorithmInterval[algo_name].value)
