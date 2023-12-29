4fd3ddb846c075aedea811b7bbb5483e8d943a85
otgon <otgon.petru@gmail.com>
2023-12-29 16:51:47 +0000

    Error handling for open position edge cases

--- src/app/trading_data/views.py
+++ src/app/trading_data/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 
 from .tasks import manage_request, schedule_algo
 from .exceptions import DuplicateAssetException
-from app.evaluation_core.models import Algorithm
+from app.evaluation_core.models import Algorithm, Asset
 from .models import Position
 from .broker import Broker
 
@@ -12,6 +12,7 @@ class OpenPositionView(APIView):
     def post(self, request, *args, **kwargs):
         algo_name = request.data.get("algo_name")
         ticker = request.data.get("ticker")
+        user = request.data.get("user") # Change to whatever method I'll use to auth
         
         if not algo_name:
             return Response({"error":"Missing algorithm"}, status=status.HTTP_400_BAD_REQUEST)
@@ -27,12 +28,15 @@ class OpenPositionView(APIView):
         
         try:
             manage_request(algo_name, ticker)
-            schedule_algo.delay(algo_name, ticker, Broker())
         except DuplicateAssetException as e:
             return Response({"error":str(e)}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({"error":str(e)}, status=status.HTTP_409_CONFLICT)
         
+        if Position.objects.filter(user=user, asset=Asset.objects.get(ticker=ticker),status=StatusChoices.OPEN):
+                return Response({"error":"An open position already exists for this asset"}, status=status.HTTP_400_BAD_REQUEST)
+            
+        schedule_algo.delay(algo_name, ticker, Broker())
         return Response({"message":f"{algo_name} algorithm successfully started for {ticker}"}, status=status.HTTP_202_ACCEPTED)
     
 class ClosePositionView(APIView):
