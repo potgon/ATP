cf8d1d933ef217170eacfc584854df586406c28a
otgon <otgon.petru@gmail.com>
2023-12-30 11:20:58 +0000

    api_utils module with func to retrieve fields from HTTP request

---
+++ src/app/utils/api_utils.py
@@ -0,0 +1,6 @@
+def get_required_fields(request, fields: list[str]) -> dict:
+    missing_fields = [field for field in fields if field not in request.data]
+    if missing_fields:
+        raise Exception(f"Missing required fields: {', '.join(missing_fields)}")
+    
+    return {field: request.data.get(field) for field in fields}
