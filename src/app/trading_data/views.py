4fe89395c219004193b2b486203781cdcae52c9b
otgon <otgon.petru@gmail.com>
2023-12-30 11:21:14 +0000

    get_required_fields implementation in views

--- src/app/trading_data/views.py
+++ src/app/trading_data/views.py
@@ -9,54 +9,49 @@ from .models import Position
 from .broker import Broker
 from .fetcher import Fetcher
 from app.utils.db_utils import retrieve_single_record, retrieve_multiple_records
+from app.utils.api_utils import get_required_fields
 
 class OpenPositionView(APIView):
     def post(self, request, *args, **kwargs):
+        req_fields = ["algo_name", "ticker", "user"] # Check user auth
         try:
-            algo_name = request.data.get("algo_name")
-            ticker = request.data.get("ticker")
-            user = request.data.get("user_id") # Change to whatever method I'll use to auth
-        except KeyError as e:
-            return Response({"error":f"Missing required field {e.args[0]}"}, status=status.HTTP_400_BAD_REQUEST)
+            fields = get_required_fields(request, req_fields) 
         except Exception as e:
             return Response({"error":str(e)}, status=status.HTTP_400_BAD_REQUEST)
         
-        if not retrieve_record(Algorithm, name=algo_name).status == "Active":
+        if not retrieve_record(Algorithm, name=fields["algo_name"]).status == "Active":
             return Response({"error":"Algorithm is not currently operative"})
-        
+
         try:
-            manage_request(algo_name, ticker)
+            manage_request(fields["algo_name"], fields["ticker"])
         except DuplicateAssetException as e:
             return Response({"error":str(e)}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({"error":str(e)}, status=status.HTTP_409_CONFLICT)
-        
-        if retrieve_multiple_records(Position, user_id=user, asset_id=retrieve_single_record(Asset, ticker=ticker), status=StatusChoices.OPEN):        
+
+        if retrieve_multiple_records(Position, user_id=fields["user"], asset_id=retrieve_single_record(Asset, ticker=fields["ticker"]), status=StatusChoices.OPEN):        
             return Response({"error":"An open position already exists for this asset"}, status=status.HTTP_400_BAD_REQUEST)
-            
-        schedule_algo.delay(algo_name, ticker, Broker())
-        return Response({"message":f"{algo_name} algorithm successfully started for {ticker}"}, status=status.HTTP_202_ACCEPTED)
-    
+
+        schedule_algo.delay(fields["algo_name"], fields["ticker"], Broker())
+        return Response({"message":f"{fields['algo_name']} algorithm successfully started for {fields['ticker']}"}, status=status.HTTP_202_ACCEPTED)
+
 class ClosePositionView(APIView):
     def delete(self, request, *args, **kwargs):
+        req_fields = ["pos_id", "user_id"]
         try:
-            pos_id = request.data.get("id")
-            user_id = request.data.get("user_id")
-        except KeyError as e:
-            return Response({"error":f"Missing required field {e.args[0]}"}, status=status.HTTP_400_BAD_REQUEST)
+            fields = get_required_fields(request, req_fields)
         except Exception as e:
             return Response({"error":str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        
-        pos = retrieve_single_record(Position, id=pos_id, user_id=user_id)
+
+        pos = retrieve_single_record(Position, id=fields["pos_id"], user_id=fields["user_id"])
         try:
             Broker().close_pos(pos)
         except Exception: # Broker's exception if position couldn't be closed
             return Response({"error":"Position couldn't be closed"},status=status.HTTP_400_BAD_REQUEST)
         try:
-            exit_price = Fetcher.get_latest_close(retrieve_single_record(Asset, id=pos.asset_id).ticker)
+            exit_price = Fetcher.get_latest_close(retrieve_single_record(Asset, id=pos.asset).ticker)
         except Exception as e:
             return Response({"error":"Failed to retrieve exit price for current asset"})
         pos.close_db(exit_price)
-        
+
         return Response({"message":"Position closed successfully"}, status=status.HTTP_202_ACCEPTED)
-        
