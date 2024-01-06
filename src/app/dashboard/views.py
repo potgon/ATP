9d00e1409e35ab65b28fc1c2a02e32f4312da960
otgon <otgon.petru@gmail.com>
2024-01-06 14:56:52 +0000

    Removed not used imports

--- src/app/dashboard/views.py
+++ src/app/dashboard/views.py
@@ -1,10 +1,5 @@
-from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
 
