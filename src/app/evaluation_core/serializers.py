a14bac99fca08ea58e407c474d706d625d45d9af
otgon <otgon.petru@gmail.com>
2024-01-06 12:30:57 +0000

    Validation in Algo Serializer

--- src/app/evaluation_core/serializers.py
+++ src/app/evaluation_core/serializers.py
@@ -5,6 +5,12 @@ class AlgorithmSerializer(serializers.ModelSerializer):
     class Meta:
         model = Algorithm
         fields = '__all__'
+        
+        def validate_algo(self, data):
+            if not Algorithm.objects.filter(name=data["algo_name"], status="Active").exists():
+                raise serializers.ValidationError("Algorithm is not currently operative")
+        
+        return data
 
 class AssetSerializer(serializers.ModelSerializer):
     class Meta:
--- src/app/evaluation_core/views.py
+++ src/app/evaluation_core/views.py
@@ -1,8 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.generics import GenericViewSet
 
 from .models import Algorithm, Asset
 from .serializers import AlgorithmSerializer, AssetSerializer
+
+class RunAlgorithmView(GenericViewSet):
+    serializer_class = AlgorithmSerializer
+    
+    @action(details=True, methods=["post"])
+    def run(self, request, *args, **kwargs):
+        ...
 
 class ListAlgorithmsView(APIView):
     def get(self, request, *args, **kwargs) -> Response:
