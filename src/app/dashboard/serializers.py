d6c583a14fcb0877accbedc73d25d5646bf5ff7a
potgon <otgon.petru@gmail.com>
2024-02-04 15:21:43 +0000

    make_password missing import

--- src/app/dashboard/serializers.py
+++ src/app/dashboard/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
