a16df01ca823362d423f3926c5e7cd605eda02dc
otgon <otgon.petru@gmail.com>
2024-03-09 14:38:49 +0100

    ARIMA trainer base model and NatgasARIMATrainer extension

---
+++ src/app/evaluation_core/model_trainers/arima_trainer.py
@@ -0,0 +1,34 @@
+from pmdarima import auto_arima
+
+from evaluation_core.model_base import ModelTrainer
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_absolute_error
+
+
+class ARIMATrainer(ModelTrainer):
+    def __init__(self, order, seasonal_order=None, auto_arima_enabled=False):
+        super().__init__()
+        self.order = order
+        self.seasonal_order = seasonal_order
+        self.auto_arima_enabled = auto_arima_enabled
+        self.trained_model = None
+
+    def train(self, data):
+        if self.auto_arima_enabled:
+            # fmt: off
+            self.trained_model = auto_arima(data, start_p=1, start_q=1,
+                                            max_p=5, max_q=5, m=12,
+                                            seasonal=True, trace=True,
+                                            error_action='ignore',  
+                                            suppress_warnings=True, 
+                                            stepwise=True)
+            # fmt: on
+        else:
+            model = ARIMA(data, order=self.order, seasonal_order=self.seasonal_order)
+            self.trained_model = model.fit()
+
+    def evaluate(self, actual, forecasted):
+        return mean_absolute_error(actual, forecasted)
+
+    def predict(self, steps):
+        return self.trained_model.forecast(steps=steps)
---
+++ src/app/evaluation_core/predictive_models/natgas_arima.py
@@ -0,0 +1,38 @@
+import yfinance as yf
+from sklearn.model_selection import train_test_split
+from joblib import dump, load
+
+from app.evaluation_core.model_trainers.arima_trainer import ARIMATrainer
+
+# ARIMA model trained with NATGAS data
+
+
+class NatgasARIMAModel:
+    def __init__(self, order, seasonal_order=None, auto_arima_enabled=False):
+        self.order = order
+        self.seasonal_order = seasonal_order
+        self.auto_arima_enabled = auto_arima_enabled
+        self.arima_trainer = ARIMATrainer(
+            order=self.order,
+            seasonal_order=self.seasonal_order,
+            auto_arima_enabled=self.auto_arima_enabled,
+        )
+        self.trained_model = None
+
+    def data_init(self, ticker="NG=F", start_date="2019-01-01", end_date="2022-01-01"):
+        data = yf.download(ticker, start=start_date, end=end_date)["Close"]
+        return data
+
+    def train(self, data):
+        self.arima_trainer(data)
+        self.trained_model = self.arima_trainer.trained_model
+
+    def save_model(self, filepath):
+        dump(self.trained_model, filepath)
+
+    def load_model(self, filepath):
+        self.trained_model = load(filepath)
+        self.arima_trainer.trained_model = self.trained_model
+
+    def predict(self, steps=5):
+        return self.arima_trainer.predict(steps)
