324cae924d86d10273b98d6fe16b1752c5b8fed2
otgon <otgon.petru@gmail.com>
2024-03-12 15:18:37 +0100

    ARIMATrainer implements auto_arima_enabled flag check for custom order parameter

--- src/app/evaluation_core/model_trainers/arima_trainer.py
+++ src/app/evaluation_core/model_trainers/arima_trainer.py
@@ -6,7 +6,7 @@ from app.evaluation_core.model_base import ModelTrainer
 
 
 class ARIMATrainer(ModelTrainer):
-    def __init__(self, order, seasonal_order=None, auto_arima_enabled=False):
+    def __init__(self, order=None, seasonal_order=None, auto_arima_enabled=False):
         super().__init__()
         self.order = order
         self.seasonal_order = seasonal_order
@@ -16,13 +16,16 @@ class ARIMATrainer(ModelTrainer):
     def train(self, data):
         if self.auto_arima_enabled:
             # fmt: off
-            self.trained_model = auto_arima(data, start_p=1, start_q=1,
-                                            max_p=5, max_q=5, m=12,
-                                            seasonal=True, trace=True,
-                                            error_action='ignore',  
-                                            suppress_warnings=True, 
-                                            stepwise=True)
+            auto_model = auto_arima(data, start_p=1, start_q=1,
+                                    max_p=5, max_q=5, m=12,
+                                    start_P=0, seasonal=True, D=1, trace=True,
+                                    error_action='ignore',  
+                                    suppress_warnings=True, 
+                                    stepwise=True)
             # fmt: on
+            self.order = auto_model.order()
+            self.seasonal_order = auto_model.seasonal_order()
+            self.trained_model = model.fit()
         else:
             model = ARIMA(data, order=self.order, seasonal_order=self.seasonal_order)
             self.trained_model = model.fit()
@@ -31,4 +34,7 @@ class ARIMATrainer(ModelTrainer):
         return mean_absolute_error(actual, forecasted)
 
     def predict(self, steps):
-        return self.trained_model.forecast(steps=steps)
+        if self.auto_arima_enabled:
+            return self.trained_model.predict(n_periods=steps)
+        else:
+            return self.trained_model.forecast(steps=steps)
