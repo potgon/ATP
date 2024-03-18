0eeec235234ad32860d06c8577e3acda0f336b65
Petru <otgon.petru@gmail.com>
2024-03-18 18:57:42 +0100

    Class name refactor

--- src/app/evaluation_core/predictive_models/sp500_lstm.py
+++ src/app/evaluation_core/predictive_models/sp500_lstm.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 
 from app.evaluation_core.model_core.window_pipeline import data_init, data_processing
 from app.evaluation_core.model_trainers.lstm_trainer import LSTMTrainer
 
-class LSTMModel:
+class SP500LSTMModel:
     def __init__(self, units=50):
         self._LSTMTrainer = LSTMTrainer(window=data_processing(self.feature_engineering()), units=units) # Refactor, ping-pong is no bueno
         self.trained_model = None
