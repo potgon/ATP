fd8fe52b65779ce814361a16629718c3f0a30d62
otgon <otgon.petru@gmail.com>
2024-03-19 16:11:57 +0100

    feature_engineering added to window_pipeline, data_init drops columns if first row is 0.0. All data proces is done in that module

--- src/app/evaluation_core/model_core/window_pipeline.py
+++ src/app/evaluation_core/model_core/window_pipeline.py
@@ -1,10 +1,27 @@
 import pandas as pd
 from src.app.evaluation_core.model_core.window_generator import WindowGenerator
 
-def data_init(filepath: str, columns_dropped: list[str] | None) -> pd.DataFrame:
+
+def data_init(filepath: str) -> pd.DataFrame:
     df = pd.read_csv(filepath)
-    df.drop(columns=columns_dropped, inplace=True)
+    columns_to_drop = df.columns[df.iloc[0] == 0.0]
+    if columns_to_drop:
+        df.drop(columns_to_drop, axis=1, inplace=True)
     return df
+
+
+def feature_engineering(df) -> pd.DataFrame:
+    date_time = pd.to_datetime(df.pop("Date"), utc=True)
+    df["day_of_week"] = date_time.dt.dayofweek
+    df["month_of_year"] = date_time.dt.month
+
+    df["day_sin"] = np.sin(df["day_of_week"] * (2 * np.pi / 7))
+    df["day_cos"] = np.cos(df["day_of_week"] * (2 * np.pi / 7))
+    df["month_sin"] = np.sin((df["month_of_year"] - 1) * (2 * np.pi / 12))
+    df["month_cos"] = np.cos((df["month_of_year"] - 1) * (2 * np.pi / 12))
+
+    return df
+
 
 def data_split(df):
     n = len(df)
@@ -13,6 +30,7 @@ def data_split(df):
     test_df = df[int(n * 0.9) :]
 
     return train_df, val_df, test_df
+
 
 def data_normalization(train_df, val_df, test_df):
     train_mean = train_df.mean()
@@ -24,14 +42,19 @@ def data_normalization(train_df, val_df, test_df):
 
     return train_df, val_df, test_df
 
-def data_processing(df):
-    train_df, val_df, test_df = data_split(df)
+
+def data_processing(filepath: str):
+    df = data_init(filepath)
+    engineered_df = feature_engineering(df)
+    train_df, val_df, test_df = data_split(engineered_df)
     train_df, val_df, test_df = data_normalization(train_df, val_df, test_df)
-    
-    return WindowGenerator(input_width=30,
-                            label_width=1,
-                            shift=1,
-                            train_df=train_df,
-                            val_df=val_df,
-                            test_df=test_df,
-                            label_columns=["Close"])
+
+    return WindowGenerator(
+        input_width=30,
+        label_width=1,
+        shift=1,
+        train_df=train_df,
+        val_df=val_df,
+        test_df=test_df,
+        label_columns=["Close"],
+    )
