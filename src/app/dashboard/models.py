ffc849174d99227b7f5e8dc1c412d5d4692411af
otgon <otgon.petru@gmail.com>
2024-03-23 14:55:19 +0100

    Custom user model

--- src/app/dashboard/models.py
+++ src/app/dashboard/models.py
@@ -1,7 +1,14 @@
+from django.utils import timezone
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
-class PriorityUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_priority = models.BooleanField(default=False)
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    priority = models.BooleanField(default=False)
