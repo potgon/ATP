f852c464651caebd8a9a1b729cd1a76481464a58
Petru <otgon.petru@gmail.com>
2024-03-25 19:00:46 +0100

    Singleton Trainer is instantiated at platform startup, subsequent calls will return this instance

--- src/app/model_core/apps.py
+++ src/app/model_core/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ModelCoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app.model_core"
+
+    def ready(self):
+        from .trainer import Trainer
+        Trainer()
