49edd1e4e35c4852addf82fa649124817dcf7189
otgon <otgon.petru@gmail.com>
2024-04-01 21:24:07 +0200

    TrainModelView endpoint that users query to send train requests, acts as Kafka producer

--- src/app/model_core/views.py
+++ src/app/model_core/views.py
@@ -1,8 +1,11 @@
+import os
+import json
 from rest_framework import status
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from confluent_kafka import Producer
 
 from app.evaluation_core.serializers import AssetSerializer
 from .models import ModelType, TrainedModel
@@ -25,16 +28,32 @@ class ListTrainedModelsView(GenericViewSet, ListModelMixin):
     def list(self, request, *args, **kwargs):
         return super(ListTrainedModelsView, self).list(request, *args, **kwargs)
 
+
 class TrainModelView(GenericViewSet, CreateModelMixin):
     permission_classes = (IsAuthenticated,)
-    
-    def post(self, request, *args, **kwargs):
+
+    def create(self, request, *args, **kwargs):
         asset_serializer = AssetSerializer(data=request.data)
         model_serializer = ModelTypeSerializer(data=request.data)
-        
+
         if asset_serializer.is_valid() and model_serializer.is_valid():
             asset = asset_serializer.validated_data["ticker"]
             model = model_serializer.validated_data["model_name"]
             user = request.user
-            
-            
+
+            msg = json.dumps({"user": user.username, "asset": asset, "model": model})
+
+            producer = Producer(
+                {"bootstrap.servers": os.getenv("KAFKA_BOOTSTRAP_SERVER")}
+            )
+
+            topic = os.getenv("MODEL_QUEUE_TRAIN_TOPIC")
+            producer.produce(topic, msg.encode("utf-8"))
+            producer.flush()
+
+            producer.close()
+
+            return Response(
+                {"message": "Model training request sent to Kafka"},
+                status=status.HTTP_201_CREATED,
+            )
