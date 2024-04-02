4bf2d53d608a5b91f93cfa2a30926efa104d1bdd
otgon <otgon.petru@gmail.com>
2024-04-02 17:04:57 +0200

    Removed trainer init at platform startup, since it's not Singleton anymore

--- src/app/model_core/apps.py
+++ src/app/model_core/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ModelCoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app.model_core"
-
-    def ready(self):
-        from .trainer import Trainer
-        Trainer()
